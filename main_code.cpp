69de63c9c13d0796dd9b2f98fb1c7a35c55ac9b2
Mumtaz Ali <167609415+alimumtazr@users.noreply.github.com>
2024-12-10 11:47:30 +0500

    initializers main_code.cpp
    -> Graphics Setup:  
    - Configured window dimensions (`WINDOW_HEIGHT`, `WINDOW_WIDTH`).  
    - Initialized textures and sprites for backgrounds, buttons, and navigation elements.  
    - Dynamic bar width calculated based on `ARRAY_SIZE`.  
    
    -> Audio Integration:  
    - Loaded sound buffers for beep, timer alert, and click sounds.  
    - Associated sound objects for playback.  
    
    -> Timer Functionality:  
    - Set up a timer using "sf::Clock" with "minutes", "seconds", and "milliseconds" tracking.  
    - Added "timerRunning" flag for control.

--- main_code.cpp
+++ main_code.cpp
@@ -1,1 +1,87 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <sstream>
+#include <windows.h>
 
+using namespace std;
+
+const int WINDOW_HEIGHT = 1200;
+const int WINDOW_WIDTH = 1000;
+const int wh = 1500;
+const int ww = 1500;
+int ARRAY_SIZE = 20;
+int speed = 50;
+int BAR_WIDTH = (WINDOW_WIDTH / 3) / (ARRAY_SIZE / 3);
+int elapsedTime = 0; // milliseconds mein hai
+int minutes = 0;
+int seconds = 0;
+int milliseconds = 0;
+bool timerRunning = false;
+
+sf::Texture bgt;
+sf::Sprite bgs;
+
+sf::Texture bot;
+sf::Sprite bos;
+
+sf::Texture solgt;
+sf::Sprite solgs;
+
+sf::Texture b1t;
+sf::Sprite b1s;
+
+sf::Texture b2t;
+sf::Sprite b2s;
+
+sf::Texture b3t;
+sf::Sprite b3s;
+
+sf::Texture b4t;
+sf::Sprite b4s;
+
+sf::Texture b5t;
+sf::Sprite b5s;
+
+sf::Texture b6t;
+sf::Sprite b6s;
+
+sf::Texture splt;
+sf::Sprite spls;
+
+sf::Texture sprt;
+sf::Sprite sprs;
+
+sf::Texture nlt;
+sf::Sprite nls;
+
+sf::Texture nrt;
+sf::Sprite nrs;
+
+sf::Texture ret;
+sf::Sprite res;
+
+sf::Texture timet;
+sf::Sprite times;
+
+sf::SoundBuffer beepBuffer;
+sf::Sound beepSound;
+
+sf::SoundBuffer tBuffer;
+sf::Sound tSound;
+
+sf::SoundBuffer clickBuffer;
+sf::Sound clickSound;
+
+sf::Text sped;
+
+sf::Text nnp;
+
+sf::Text sp;
+
+sf::Text timerText;
+
+sf::Clock timerClock; 

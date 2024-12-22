6de63f0765d2321ad7ee27cd3d40dc81829fa253
Daanish Ahmad Mufti <79512809+daanishmufti@users.noreply.github.com>
2024-12-22 23:18:11 +0500

    Update main_code.cpp
    This code implements a sorting visualizer using SFML. It displays an array as colored bars, where each bar's height represents the value of an array element. The visualizer animates sorting algorithms with:
    
    Graphics: Bars change color during sorting (e.g., red for the current comparison, green for sorted elements).
    Timer: Displays elapsed time for the sorting process.
    Sound Effects: Plays sounds during specific sorting events.
    Key functions:
    
    render(): Draws background, buttons, and the timer.
    drawArray(): Animates and colors the array bars dynamically based on the sorting status.
    updateTimer(): Updates and displays the elapsed time.
    The application is interactive, visually representing sorting progress and allowing users to understand algorithms better.

--- main_code.cpp
+++ main_code.cpp
@@ -6,12 +6,21 @@
 #include <ctime>
 #include <sstream>
 #include <windows.h>
+#include <iostream>
+#include <queue>
+#include <cmath>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <unordered_map>
+
 
 using namespace std;
+using namespace sf;
 
 const int WINDOW_HEIGHT = 1200;
 const int WINDOW_WIDTH = 1000;
-const int wh = 1500;
+const int wh = 1200;
 const int ww = 1500;
 int ARRAY_SIZE = 20;
 int speed = 50;
@@ -25,11 +34,14 @@ bool timerRunning = false;
 sf::Texture bgt;
 sf::Sprite bgs;
 
+sf::Texture bfst;
+sf::Sprite bfss;
+
+sf::Texture hmpt;
+sf::Sprite hmps;
+
 sf::Texture bot;
 sf::Sprite bos;
-
-sf::Texture solgt;
-sf::Sprite solgs;
 
 sf::Texture b1t;
 sf::Sprite b1s;
@@ -76,7 +88,6 @@ sf::Sound tSound;
 sf::SoundBuffer clickBuffer;
 sf::Sound clickSound;
 
-sf::Text sped;
 
 sf::Text nnp;
 
@@ -84,17 +95,18 @@ sf::Text sp;
 
 sf::Text timerText;
 
-sf::Clock timerClock; 
-string intToString(int value)
+sf::Clock timerClock;
+
+std::string intToString(int value)
 {
-    ostringstream oss;
+    std::ostringstream oss;
     oss << value;
     return oss.str();
 }
 
 void updateTimer()
 {
-    if(timerRunning)
+    if (timerRunning)
     {
         sf::Time deltaTime = timerClock.restart();
         elapsedTime += deltaTime.asMilliseconds();
@@ -106,11 +118,13 @@ void updateTimer()
         timerText.setString(intToString(minutes) + "m " + intToString(seconds) + "s " + intToString(milliseconds) + "ms");
     }
 }
-void render(sf::RenderWindow &window)
+
+void render(sf::RenderWindow& window)
 {
-	window.draw(bgs);
+    window.draw(bgs);
     window.draw(bos);
-    window.draw(solgs);
+    window.draw(bfss);
+    window.draw(hmps);
     window.draw(b1s);
     window.draw(b2s);
     window.draw(b3s);
@@ -125,27 +139,27 @@ void render(sf::RenderWindow &window)
     window.draw(nnp);
     window.draw(res);
     window.draw(times);
-    window.draw(sped);
+
     window.draw(timerText);
 }
 
-void drawArray(sf::RenderWindow &window, const std::vector<int> &array, int id1 , int id2, bool q, bool f)
+void drawArray(sf::RenderWindow& window, const std::vector<int>& array, int id1, int id2, bool q, bool f)
 {
     window.clear();
     render(window);
-    if(f == false)
+    if (f == false)
     {
-    	updateTimer();
-	}
-    for(int i = 0; i < array.size(); i++)
+        updateTimer();
+    }
+    for (int i = 0; i < array.size(); i++)
     {
         sf::RectangleShape bar(sf::Vector2f(BAR_WIDTH - 2, array[i]));
-        if(i == id2)
-		{
+        if (i == id2)
+        {
             bar.setFillColor(sf::Color::Green);
         }
-		else if(i == id1)
-		{
+        else if (i == id1)
+        {
             bar.setFillColor(sf::Color::Red);
             tSound.play();
         }
@@ -156,514 +170,15 @@ void drawArray(sf::RenderWindow &window, const std::vector<int> &array, int id1 
         bar.setPosition(i * BAR_WIDTH + 50, WINDOW_HEIGHT - array[i] + 50);
         window.draw(bar);
     }
-    if(q == true)
+    if (q == true)
     {
-    	for(int i = 0 ; i < array.size() ; i++)
-    	{
-    		sf::RectangleShape bar(sf::Vector2f(BAR_WIDTH - 2, array[i]));
+        for (int i = 0; i < array.size(); i++)
+        {
+            sf::RectangleShape bar(sf::Vector2f(BAR_WIDTH - 2, array[i]));
             bar.setFillColor(sf::Color::Green);
             bar.setPosition(i * BAR_WIDTH + 50, WINDOW_HEIGHT - array[i] + 50);
             window.draw(bar);
-		}
-	}
+        }
+    }
     window.display();
-}
-void bubbleSort(sf::RenderWindow &window, vector<int> &array)
-{
-    for(int i = 0 ; i < array.size() - 1 ; i++)
-    {
-        for(int j = 0 ; j < array.size() - i - 1 ; j++)
-        {
-        	if(!window.isOpen())
-            {
-                beepSound.stop(); 
-                return;   
-            }
-            if(array[j] > array[j + 1])
-            {
-                int temp = array[j + 1];
-                array[j + 1] = array[j];
-                array[j] = temp;
-            }
-            drawArray(window, array , j , j + 1 , false , false);
-            beepSound.play();
-            sf::Event event;
-            while(window.pollEvent(event))
-            {
-                if(event.type == sf::Event::Closed)
-                {   
-                    beepSound.stop();
-                    window.close();
-                }
-            }
-            sleep(sf::milliseconds(speed));
-        }
-    }
-}
-
-void mergeSort(sf::RenderWindow &window, vector<int>& array)
-{
-    int n = array.size();
-    vector<int> temp(n);
-
-    for(int size = 1 ; size < n ; size *= 2)
-	{
-        for(int leftStart = 0 ; leftStart < n ; leftStart += 2 * size)
-		{
-            int mid = min(leftStart + size - 1 , n - 1);
-            int rightEnd = min(leftStart + 2 * size - 1 , n - 1);
-            int i = leftStart;
-            int j = mid + 1;
-            int k = leftStart;
-
-            while(i <= mid && j <= rightEnd)
-			{
-                if(array[i] <= array[j])
-				{
-                    temp[k++] = array[i++];
-                }
-				else
-				{
-                    temp[k++] = array[j++];
-                }
-            }
-
-            while(i <= mid)
-			{
-                temp[k++] = array[i++];
-            }
-
-            while(j <= rightEnd)
-			{
-                temp[k++] = array[j++];
-            }
-
-            for(int m = leftStart; m <= rightEnd; m++)
-			{
-                array[m] = temp[m];
-            }
-
-            drawArray(window, array, leftStart, rightEnd, false , false);
-            beepSound.play();
-            sf::Event event;
-            while(window.pollEvent(event))
-			{
-                if(event.type == sf::Event::Closed)
-				{
-					beepSound.stop();
-                    window.close();
-                    return; 
-                }
-            }
-            sleep(sf::milliseconds(speed));
-        }
-    }
-}
-
-void insertionSort(sf::RenderWindow &window, vector<int>& array)
-{
-    int n = array.size();
-    
-    for(int i = 1 ; i < n ; i++)
-    {
-    	int j = i - 1;
-    	int k = array[i];
-    	while(j >= 0 && array[j] > k)
-    	{
-    		array[j + 1] = array[j];
-    		j--;
-    		drawArray(window, array, i, j + 1, false , false);
-    		beepSound.play();
-    		sf::Event event;
-            while(window.pollEvent(event))
-			{
-                if(event.type == sf::Event::Closed)
-				{
-					beepSound.stop();
-                    window.close();
-                    return; 
-                }
-            }
-            sleep(sf::milliseconds(speed));
-		}
-		array[j + 1] = k;
-	}
-}
-
-void selectionSort(sf::RenderWindow &window, vector<int>& array)
-{
-    int n = array.size();
-    vector<int> temp(n);
-    
-	for(int i = 0 ; i < n - 1 ; i++)
-	{
-		int k = i;
-		for(int j = i + 1 ; j < n ; j++)
-		{
-			if(array[j] < array[k])
-			{
-				k = j;
-			}
-			drawArray(window, array, i , j , false , false);
-			beepSound.play();
-    		sf::Event event;
-            while(window.pollEvent(event))
-			{
-                if(event.type == sf::Event::Closed)
-				{
-					beepSound.stop();
-                    window.close();
-                    return; 
-                }
-            }
-            sleep(sf::milliseconds(speed));
-		}
-		int temp = array[i];
-		array[i] = array[k];
-		array[k] = temp;
-	}
-}
-void quickSort(sf::RenderWindow &window, vector<int>& array)
-{
-    vector<int> indices;
-    indices.push_back(0);
-    indices.push_back(array.size() - 1);
-
-    while(!indices.empty())
-	{
-        int high = indices.back(); indices.pop_back();
-        int low = indices.back(); indices.pop_back();
-
-        if(low < high)
-		{
-            int pivot = array[high];
-            int i = low - 1;
-
-            for(int j = low; j < high; j++)
-			{
-                if(array[j] < pivot)
-				{
-                    i++;
-                    std::swap(array[i], array[j]);
-                    drawArray(window, array, i, j, false , false);
-
-                    sf::Event event;
-                    while (window.pollEvent(event))
-					{
-                        if(event.type == sf::Event::Closed)
-						{
-							beepSound.stop();
-                            window.close();
-                            return;
-                        }
-                    }
-                    sleep(sf::milliseconds(speed));
-                }
-            }
-            int temp = array[i + 1];
-            array[i + 1] = array[high];
-            array[high] = temp;
-            drawArray(window, array, i + 1, high, false , false);
-            beepSound.play();
-            sf::Event event;
-            while(window.pollEvent(event))
-			{
-                if(event.type == sf::Event::Closed)
-				{
-					beepSound.stop();
-                    window.close();
-                    return;
-                }
-            }
-            sleep(sf::milliseconds(speed));
-
-            int pivotIndex = i + 1;
-
-            indices.push_back(low);
-            indices.push_back(pivotIndex - 1); 
-            indices.push_back(pivotIndex + 1);
-            indices.push_back(high); 
-        }
-    }
-}
-
-void shellSort(sf::RenderWindow &window, vector<int>& array)
-{
-    int n = array.size();
-    int gap = n / 2; 
-    while(gap > 0)
-	{
-        for(int i = gap; i < n; i++)
-		{
-            int temp = array[i];
-            int j = i;
-            while(j >= gap && array[j - gap] > temp)
-			{
-                array[j] = array[j - gap];
-                drawArray(window , array , j , j - gap , false , false);
-                beepSound.play();
-                sf::Event event;
-                while(window.pollEvent(event))
-				{
-                    if(event.type == sf::Event::Closed)
-					{
-						beepSound.stop();
-                        window.close();
-                        return;
-                    }
-                }
-                sleep(sf::milliseconds(speed));
-                j -= gap;
-            }
-            array[j] = temp;
-            drawArray(window , array , j , i , false , false);
-            beepSound.play();
-            sf::Event event;
-            while(window.pollEvent(event))
-			{
-                if(event.type == sf::Event::Closed)
-                {
-                	beepSound.stop();
-                    window.close();
-                    return;
-                }
-            }
-            sleep(sf::milliseconds(speed));
-        }
-        gap /= 2; 
-    }
-}
-void software(sf::RenderWindow &window)
-{
-    window.draw(timerText);
-    BAR_WIDTH = WINDOW_WIDTH / ARRAY_SIZE;
-    vector<int> array(ARRAY_SIZE);
-    bool done = false;
-
-    for(int i = 0; i < ARRAY_SIZE; i++)
-    {
-        array[i] = std::rand() % (WINDOW_HEIGHT - 200) + 20;
-    }
-
-    timerText.setString(intToString(minutes) + "m " + intToString(seconds) + "s " + intToString(milliseconds) + "ms");
-
-    while(window.isOpen())
-    {
-        drawArray(window, array, -1, -1, false, true);
-
-        sf::Event event;
-        while(window.pollEvent(event))
-        {
-            if(event.type == sf::Event::Closed)
-            {
-                beepSound.stop();
-                window.close();
-            }
-	    else if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
-            {
-                if(b1s.getGlobalBounds().contains(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y)))
-                {
-                    clickSound.play();
-                    sped.setString("Bubble");
-                    if(done == true)
-                    {
-                        for(int i = 0; i < ARRAY_SIZE; i++)
-                        {
-                            array[i] = std::rand() % (WINDOW_HEIGHT - 200) + 20;
-                        }
-                        drawArray(window, array, 0, 0, false, true);
-                        sleep(sf::milliseconds(500));
-                    }
-                    elapsedTime = 0;
-                    timerClock.restart();
-                    timerRunning = true;
-                    bubbleSort(window, array);
-                    timerRunning = false;
-                    sleep(sf::milliseconds(500));
-                    for(int i = 0; i < ARRAY_SIZE; i++)
-                    {
-                        drawArray(window, array, -1, i, false, true);
-                        sleep(sf::milliseconds(50));
-                        if(window.isOpen())
-                        {
-                            beepSound.play();
-                        }
-                    }
-                    sleep(sf::milliseconds(500));
-                    done = true;
-                }
-else if(b2s.getGlobalBounds().contains(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y)))
-                {
-                    clickSound.play();
-                    sped.setString("Selection");
-                    if(done == true)
-                    {
-                        for(int i = 0; i < ARRAY_SIZE; i++)
-                        {
-                            array[i] = std::rand() % (WINDOW_HEIGHT - 200) + 20;
-                        }
-                        drawArray(window, array, 0, 0, false, true);
-                        sleep(sf::milliseconds(500));
-                    }
-                    elapsedTime = 0;
-                    timerClock.restart();
-                    timerRunning = true;
-                    selectionSort(window, array);
-                    timerRunning = false;
-                    sleep(sf::milliseconds(500));
-                    for(int i = 0; i < ARRAY_SIZE; i++)
-                    {
-                        drawArray(window, array, -1, i, false, true);
-                        sleep(sf::milliseconds(50));
-                        if(window.isOpen())
-                        {
-                            beepSound.play();
-                        }
-                    }
-                    sleep(sf::milliseconds(500));
-                    done = true;
-                }
-                else if(b3s.getGlobalBounds().contains(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y)))
-                {
-                    clickSound.play();
-                    sped.setString("Insertion");
-                    if(done == true)
-                    {
-                        for(int i = 0; i < ARRAY_SIZE; i++)
-                        {
-                            array[i] = std::rand() % (WINDOW_HEIGHT - 200) + 20;
-                        }
-                        drawArray(window, array, 0, 0, false, true);
-                        sleep(sf::milliseconds(500));
-                    }
-                    elapsedTime = 0;
-                    timerClock.restart();
-                    timerRunning = true;
-                    insertionSort(window, array);
-                    timerRunning = false;
-                    sleep(sf::milliseconds(500));
-                    for(int i = 0; i < ARRAY_SIZE; i++)
-                    {
-                        drawArray(window, array, -1, i, false, true);
-                        sleep(sf::milliseconds(50));
-                        if(window.isOpen())
-                        {
-                            beepSound.play();
-                        }
-                    }
-                    sleep(sf::milliseconds(500));
-                    done = true;
-                }
-            }
-                else if (b4s.getGlobalBounds().contains(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y)))
-                {
-                    clickSound.play();
-                    sped.setString("Merge");
-                    // Merge sort logic
-                }
-                else if (b5s.getGlobalBounds().contains(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y)))
-                {
-                    clickSound.play();
-                    sped.setString("Shell");
-                    // Shell sort logic
-                }
-                else if (b6s.getGlobalBounds().contains(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y)))
-                {
-                    clickSound.play();
-                    sped.setString("Quick");
-                    // Quick sort logic
-                }
-                else if (spls.getGlobalBounds().contains(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y)))
-                {
-                    clickSound.play();
-                    speed -= 10;
-                    if (speed == 0) speed = 10;
-                }
-                else if (sprs.getGlobalBounds().contains(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y)))
-                {
-                    clickSound.play();
-                    speed += 10;
-                    if (speed == 510) speed = 500;
-                }
-                else if (res.getGlobalBounds().contains(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y)))
-                {
-                    clickSound.play();
-                    software(window); // Restart logic
-                }
-            }
-        }
-        if (done)
-        {
-            drawArray(window, array, 0, 0, true, true);
-            sleep(sf::milliseconds(500));
-        }
-    }
-int main()
-{
-sf::RenderWindow window(sf::VideoMode(ww, wh), "Sorting Visualizer");
-srand(static_cast<unsigned>(time(NULL)));
-
-beepBuffer.loadFromFile("write.wav");
-beepSound.setBuffer(beepBuffer);
-beepSound.setVolume(20);
-
-tBuffer.loadFromFile("read.wav");
-tSound.setBuffer(tBuffer);
-tSound.setVolume(20);
-
-clickBuffer.loadFromFile("bt.wav");
-clickSound.setBuffer(clickBuffer);
-clickSound.setVolume(20);
-bgt.loadFromFile("bg1.png");
-bgs.setTexture(bgt);
-bgs.setScale(1.5, 1.5);
-
-bot.loadFromFile("board.png");
-bos.setTexture(bot);
-bos.setScale(1.52, 1.15);
-bos.setPosition(5, 150);
-
-solgt.loadFromFile("lgo.png");
-solgs.setTexture(solgt);
-solgs.setScale(1, 0.6);
-solgs.setPosition(-650, -200);
-
-b1t.loadFromFile("b11.png");
-b1s.setTexture(b1t);
-b1s.setScale(1.1, 1.2);
-b1s.setPosition(1095, 250);
-
-sf::Font font;
-font.loadFromFile("art.ttf");
-
-sped.setFont(font);
-sped.setString("");
-sped.setCharacterSize(80); 
-sped.setFillColor(sf::Color::Yellow);
-sped.setStyle(sf::Text::Bold);
-sped.move(1170, 130);
-
-sp.setFont(font);
-sp.setString(intToString(speed));
-sp.setCharacterSize(80); 
-sp.setFillColor(sf::Color::Yellow);
-sp.setStyle(sf::Text::Bold);
-sp.move(1250, 1295);
-
-nnp.setFont(font);
-nnp.setString(intToString(ARRAY_SIZE));
-nnp.setCharacterSize(80); 
-nnp.setFillColor(sf::Color::Yellow);
-nnp.setStyle(sf::Text::Bold);
-nnp.move(1250, 1390);
-
-timerText.setFont(font);
-timerText.setString(intToString(minutes) + "m " + intToString(seconds) + "s " + intToString(milliseconds) + "ms");
-timerText.setCharacterSize(40); 
-timerText.setFillColor(sf::Color::Black);
-timerText.setStyle(sf::Text::Bold);
-timerText.move(360, 1340);
-
-software(window);
-return 0;
 }
